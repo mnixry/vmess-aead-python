11d2651cc665dc3500e54878a5c58abe8f273f3a
Mix <32300164+mnixry@users.noreply.github.com>
2023-12-22 22:15:48 +0800

    :white_check_mark: Add test case for reading body

--- tests/test_unpack.py
+++ tests/test_unpack.py
@@ -24,5 +24,15 @@ def test_full_header():
     data = bytes.fromhex(Path(__file__).parent.joinpath("test.hex").read_text())
     user_id = uuid.UUID("b831381d-6324-4d53-ad4f-8cda48b30811")
     read = VMessAEADPacketHeader.read(data, user_id, timestamp=1703242500)
+
     assert read.payload.address == IPv4Address("104.26.12.31")
     assert read.payload.port == 80
+    body = [*read.read_body(data[read.read_offset :])]
+    assert body == [
+        b"GET / HTTP/1.1\r\nHost: ip.sb\r\nUser-Agent: curl/8.5.0\r\nAccept: */*\r\n\r\n"
+    ]
+
+
+if __name__ == "__main__":
+    test_kdf()
+    test_full_header()
