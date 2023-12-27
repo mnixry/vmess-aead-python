dba973505c26ab9cab5dc75b06ef4c36b3f854d6
Mix <32300164+mnixry@users.noreply.github.com>
2023-12-27 15:37:26 +0800

    :white_check_mark: Add tests for utils and kdf components

---
+++ tests/test_00_components.py
@@ -0,0 +1,131 @@
+import socket
+from io import BytesIO
+
+import pytest
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+from vmess_aead.kdf import kdf
+from vmess_aead.utils import SM4GCM
+from vmess_aead.utils.reader import (
+    BufferedReader,
+    BytesReader,
+    IOReader,
+    ReadOutOfBoundError,
+    SocketReader,
+    StreamCipherReader,
+)
+
+
+def test_kdf():
+    derived_key = kdf(
+        b"Demo Key for KDF Value Test",
+        [
+            b"Demo Path for KDF Value Test",
+            b"Demo Path for KDF Value Test2",
+            b"Demo Path for KDF Value Test3",
+        ],
+    )
+    assert derived_key == bytes.fromhex(
+        "53e9d7e1bd7bd25022b71ead07d8a596efc8a845c7888652fd684b4903dc8892"
+    ), "should generate correct key"
+
+
+def test_bytes_reader():
+    reader = BytesReader(b"1234567890")
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read_before() == b"12345"
+    assert reader.read_all() == b"67890"
+
+    with pytest.raises(ReadOutOfBoundError):
+        reader.read(5)
+
+
+def test_buffered_reader():
+    reader = BufferedReader(BytesReader(b"1234567890"))
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read_before() == b"12345"
+    assert reader.read(5) == b"67890"
+
+    with pytest.raises(ReadOutOfBoundError):
+        reader.read(5)
+
+
+def test_io_reader():
+    reader = IOReader(BytesIO(b"1234567890"))
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read_all() == b"67890"
+
+    with pytest.raises(ReadOutOfBoundError):
+        reader.read(5)
+
+
+def test_stream_cipher_reader():
+    key = b"1234567890123456"
+    iv = b"1234567890123456"
+    cipher = Cipher(algorithms.AES(key), modes.CTR(iv)).decryptor()
+    reader = StreamCipherReader(
+        BytesReader(bytes.fromhex("444efe38e96aa7d2e2de")), cipher
+    )
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read(5) == b"67890"
+
+    with pytest.raises(ReadOutOfBoundError):
+        reader.read(5)
+
+
+def test_socket_reader():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("localhost", 0))
+    server.listen()
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(server.getsockname())
+    client.send(b"1234567890")
+    client.close()
+
+    connection, src_addr = server.accept()
+    reader = SocketReader(connection)
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read(5) == b"67890"
+
+    with pytest.raises(ReadOutOfBoundError):
+        reader.read(5)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(server.getsockname())
+    client.send(b"1234567890")
+    client.close()
+
+    connection, src_addr = server.accept()
+    reader = SocketReader(connection)
+    assert reader.read(5) == b"12345"
+    assert reader.offset == 5
+    assert reader.read_all() == b"67890"
+
+
+def test_sm4_gcm():
+    # test vector from https://tools.ietf.org/html/rfc8998#appendix-A.1
+    key = bytes.fromhex("0123456789ABCDEFFEDCBA9876543210")
+    nonce = bytes.fromhex("00001234567800000000ABCD")
+    associated_data = bytes.fromhex("FEEDFACEDEADBEEFFEEDFACEDEADBEEFABADDAD2")
+    plaintext = bytes.fromhex(
+        "AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB"
+        "CCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDD"
+        "EEEEEEEEEEEEEEEEFFFFFFFFFFFFFFFF"
+        "EEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAA"
+    )
+    ciphertext = bytes.fromhex(
+        "17F399F08C67D5EE19D0DC9969C4BB7D"
+        "5FD46FD3756489069157B282BB200735"
+        "D82710CA5C22F0CCFA7CBF93D496AC15"
+        "A56834CBCF98C397B4024A2691233B8D"
+    )
+
+    sm4gcm = SM4GCM(key)
+    assert sm4gcm.encrypt(nonce, plaintext, associated_data) == ciphertext
+    assert sm4gcm.decrypt(nonce, ciphertext, associated_data) == plaintext
--- tests/test_02_unpack.py
+++ tests/test_02_unpack.py
@@ -11,22 +11,7 @@ from vmess_aead.enums import (
     VMessBodySecurity,
 )
 from vmess_aead.headers.request import VMessAuthID, VMessPlainPacketHeader
-from vmess_aead.kdf import kdf
 from vmess_aead.utils.reader import BufferedReader, IOReader
-
-
-def test_kdf():
-    derived_key = kdf(
-        b"Demo Key for KDF Value Test",
-        [
-            b"Demo Path for KDF Value Test",
-            b"Demo Path for KDF Value Test2",
-            b"Demo Path for KDF Value Test3",
-        ],
-    )
-    assert derived_key == bytes.fromhex(
-        "53e9d7e1bd7bd25022b71ead07d8a596efc8a845c7888652fd684b4903dc8892"
-    ), "should generate correct key"
 
 
 def test_full_header():
@@ -76,8 +61,3 @@ def test_full_header():
         body
         == b"GET / HTTP/1.1\r\nHost: ip.sb\r\nUser-Agent: curl/8.5.0\r\nAccept: */*\r\n\r\n"
     )
-
-
-if __name__ == "__main__":
-    test_kdf()
-    test_full_header()
